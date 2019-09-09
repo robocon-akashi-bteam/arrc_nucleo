0a6ce83d114dc53dd0105e1df643eb57b5178521
Kitasola <e1611@s.akashi.ac.jp>
2019-09-10 02:12:12 +0900

    PID制御のI成分発振対策

--- library/arrc/pid.hpp
+++ library/arrc/pid.hpp
@@ -5,15 +5,16 @@
 namespace arrc {
 class PidVelocity {
 public:
-  PidVelocity(double p, double i, double d) {
-    setParam(p, i, d);
+  PidVelocity(double p, double i, double d, double max_i) {
+    setParam(p, i, d, max_i);
     time_ = new Timer;
     time_->start();
   }
-  void setParam(double p, double i, double d) {
+  void setParam(double p, double i, double d, double max_i) {
     p_gain_ = p;
     i_gain_ = i;
     d_gain_ = d;
+    max_i_control_ = max_i;
   }
   double control(double goal, double out) {
     delta_t_ = time_->read();
@@ -23,6 +24,12 @@ public:
     current_error_ = goal - out;
     double p_item = p_gain_ * (current_error_ - prev_error_);
     double i_item = i_gain_ * current_error_ * delta_t_;
+    if (i_item > max_i_control_) {
+      i_item = max_i_control_;
+    } else if (i_item < -max_i_control_) {
+      i_item = -max_i_control_;
+    }
+
     double d_item =
         d_gain_ *
         ((current_error_ - prev_error_) - (prev_error_ - prev_prev_error_)) /
@@ -38,6 +45,7 @@ private:
   double delta_t_;
   double control_ = 0;
   double current_error_ = 0, prev_error_ = 0, prev_prev_error_ = 0;
+  double max_i_control_ = 0;
 };
 } // namespace arrc
 
