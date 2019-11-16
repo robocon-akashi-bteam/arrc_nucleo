f7937f23957d266927966ba42a8ed0da0c95d101
Kitasola <e1611@s.akashi.ac.jp>
2019-11-16 12:20:16 +0900

    PCSerialデバッグ用に書換中

--- library/f446re/arrc/scrp_slave.cpp
+++ library/f446re/arrc/scrp_slave.cpp
@@ -3,57 +3,22 @@
 constexpr int STX = 0x41;
 constexpr int DMY = 0xff;
 
-ScrpSlave::ScrpSlave(PinName TX1, PinName RX1, uint32_t addr) : address_(addr) {
-  mode_ = 0;
-  init(TX1, RX1);
-}
-
-ScrpSlave::ScrpSlave(PinName TX1, PinName RX1, PinName REDE1, uint32_t addr)
-    : address_(addr) {
+ScrpSlave::ScrpSlave(PinName tx_pin, PinName rx_pin, PinName rede_pin,
+                     uint32_t baudrate, uint32_t flash_addr)
+    : timeout_(timeout), serial_(tx_pin, rx_pin, baudrate), rede_(rede_pin) {
   mode_ = 1;
-  rede_ = new DigitalOut(REDE1);
-  init(TX1, RX1);
-}
-
-ScrpSlave::ScrpSlave(PinName TX1, PinName RX1, PinName TX2, PinName RX2,
-                     uint32_t addr)
-    : address_(addr) {
-  mode_ = 2;
-  serial_[1] = new Serial(TX2, RX2, 115200);
-  serial_[1]->attach(callback(this, &ScrpSlave::port2), Serial::RxIrq);
-  init(TX1, RX1);
-}
-
-ScrpSlave::ScrpSlave(PinName TX1, PinName RX1, PinName REDE1, PinName TX2,
-                     PinName RX2, uint32_t addr)
-    : address_(addr) {
-  mode_ = 3;
-  rede_ = new DigitalOut(REDE1);
-  serial_[1] = new Serial(TX2, RX2, 115200);
-  serial_[1]->attach(callback(this, &ScrpSlave::port2), Serial::RxIrq);
-  init(TX1, RX1);
-}
-
-void ScrpSlave::init(PinName TX, PinName RX) {
-  timeout_ = 100;
-  serial_[0] = new Serial(TX, RX, 115200);
-  serial_[0]->attach(callback(this, &ScrpSlave::port1), Serial::RxIrq);
-  flash_ = new FlashIAP;
-  if (flash_->init() == 0) {
-    if (flash_->read(&my_id_, address_, 1) != 0) {
-      my_id_ = 0;
+  /* serial_[0]->attach(callback(this, &ScrpSlave::port1), Serial::RxIrq); */
+  if (flash_.init() == 0) {
+    if (flash_.read(&id_, address_, 1) != 0) {
+      id_ = 0;
     }
   } else {
-    my_id_ = 0;
+    id_ = 0;
   }
-  for (int i = 1; i < 255; ++i) {
-    procs_[i] = 0;
+  for (int i = 1; i <= MAX_NUM_CMD; ++i) {
+    procs_[i] = NULL;
   }
 }
-
-void ScrpSlave::port1() { check(0); }
-
-void ScrpSlave::port2() { check(1); }
 
 void ScrpSlave::addCMD(uint8_t cmd,
                        bool (*proc)(int cmd, int rx_data, int &tx_data)) {
@@ -70,16 +35,6 @@ void ScrpSlave::changeID(uint8_t id) {
 }
 
 int ScrpSlave::send(uint8_t id, uint8_t cmd, int16_t tx_data) {
-  return sending(0, id, cmd, tx_data);
-}
-
-int ScrpSlave::send2(uint8_t id, uint8_t cmd, int16_t tx_data) {
-  if (mode_ < 2)
-    return -1;
-  return sending(1, id, cmd, tx_data);
-}
-
-int ScrpSlave::sending(int port, uint8_t id, uint8_t cmd, int16_t tx_data) {
   uint8_t tx_dataL = tx_data;
   uint8_t tx_dataH = tx_data >> 8;
   uint8_t tx_sum = id + cmd + tx_dataL + tx_dataH;
@@ -135,7 +90,7 @@ int ScrpSlave::sending(int port, uint8_t id, uint8_t cmd, int16_t tx_data) {
   return (rx[2] + (rx[3] << 8));
 }
 
-void ScrpSlave::check(int port) {
+void ScrpSlave::update() {
   uint8_t rx_cmd;
   int16_t rx_data;
   bool received = false;
--- library/f446re/arrc/scrp_slave.hpp
+++ library/f446re/arrc/scrp_slave.hpp
@@ -2,58 +2,25 @@
 #define SCRP_SLAVE_H
 #include "mbed.h"
 
-/*USBでPCにつなぐポートと、基板上でRasPiとつなぐポートを同時に開く。
- *RedePinの有り無しの選択、ポートを一つだけ開くことも可。
- *以下から選択。
- *ScrpSlave(PinName TX1,PinName RX1,uint32_t addr);//RedePinなし、１ポート
- *ScrpSlave(PinName TX1,PinName RX1,PinName REDE1,uint32_t
- *addr);//RedePinあり、１ポート ScrpSlave(PinName TX1,PinName RX1,PinName
- *TX2,PinName RX2,uint32_t addr);//RedePinなし、２ポート ScrpSlave(PinName
- *TX1,PinName RX1,PinName REDE1,PinName TX2,PinName RX2,uint32_t
- *addr);//RedePinあり、１ポート＋RedePinなし、１ポート example not usb port
- *L432KC : TX = PA_9 , RX = PA_10 , REDE = PA_12 , addr = 0x0803e000
- *F446RE : TX = PC_12 , RX = PD_2 , RDDE = PH_1 , addr = 0x0807ffff
- */
-// ScrpSlave slave(SERIAL_TX,SERIAL_RX);
-
-inline int constrain(int x, int a, int b) {
-  return (x < a ? a : x > b ? b : x);
-}
-
-inline double constrain(double x, double a, double b) {
-  return (x < a ? a : x > b ? b : x);
-}
-
 class ScrpSlave {
 public:
-  ScrpSlave(PinName TX1, PinName RX1, uint32_t addr); // RedePinなし、１ポート
-  ScrpSlave(PinName TX1, PinName RX1, PinName REDE1,
-            uint32_t addr); // RedePinあり、１ポート
-  ScrpSlave(PinName TX1, PinName RX1, PinName TX2, PinName RX2,
-            uint32_t addr); // RedePinなし、２ポート
-  ScrpSlave(PinName TX1, PinName RX1, PinName REDE1, PinName TX2, PinName RX2,
-            uint32_t addr); // RedePinあり、１ポート＋RedePinなし、１ポート
+  ScrpSlave(PinName TX1, PinName RX1, PinName REDE1, uint32_t baudrate,
+            uint32_t timeout);
   ~ScrpSlave();
-  void setTimeout(int);
   void addCMD(uint8_t cmd, bool (*proc)(int cmd, int rx_data, int &tx_data));
-  int send(uint8_t id, uint8_t cmd, int16_t tx_data);
-  int send2(uint8_t id, uint8_t cmd, int16_t tx_data);
+  void update();
 
 private:
-  DigitalOut *rede_;
-  Serial *serial_[2];
-  FlashIAP *flash_;
-  uint8_t mode_;
-  uint8_t my_id_;
-  uint32_t address_;
-  int timeout_;
+  int send(uint8_t id, uint8_t cmd, int16_t tx_data);
+  DigitalOut rede_;
+  Serial serial_;
+  FlashIAP flash_;
+  uint8_t id_;
+  static constexpr uint32_t address_ = ;
+  uint32_t timeout_;
+  static constexpr int MAX_NUM_CMD = 254;
   bool (*procs_[256])(int cmd, int rx_data, int &tx_data);
-  int sending(int, uint8_t, uint8_t, int16_t);
   void changeID(uint8_t);
-  void check(int port);
-  void init(PinName, PinName);
-  void port1();
-  void port2();
 };
 
 #endif
